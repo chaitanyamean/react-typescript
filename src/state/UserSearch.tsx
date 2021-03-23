20410f528af077a56f9522815c261833eefe9ced
chaitanyamean <chaitanyamean@gmail.com>
2021-03-23 13:01:27 +0530

    input event file creation

--- src/state/UserSearch.tsx
+++ src/state/UserSearch.tsx
@@ -31,10 +31,7 @@ const UserSearch:React.FC = () => {
 
     return <div>
         User Search
-
-
         <input value={name} onChange={(e) => setName(e.target.value)}/>
-
         <button onClick={onClick}>Find User</button>
 
         <div>
